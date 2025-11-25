f90eee4b27b18d9963fa8b6132114c4452fca8a7
Louis Suo <louis.suo@myob.com>
2025-11-25 20:58:33 +1000

    fix compile error

--- Kids Allowance Tracker/Kids Allowance Tracker/ViewModels/CatDecoration.swift
+++ Kids Allowance Tracker/Kids Allowance Tracker/ViewModels/CatDecoration.swift
@@ -1,5 +1,17 @@
 import SwiftUI
 
+/// A simple triangle shape for the cat's nose
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        return path
+    }
+}
+
 /// Cute cat decorations to use throughout the app
 struct CatDecoration: View {
     enum Style {
@@ -83,7 +95,7 @@ struct CatDecoration: View {
                 Triangle()
                     .fill(color)
                     .frame(width: size * 0.12, height: size * 0.1)
-                    .rotationEffect(.degrees(180))
+                    .rotationEffect(Angle.degrees(180))
                 
                 // Smile
                 Path { path in
